28388d880237ebc0dcca0bd03befaff61d7ea37e
reannu123 <reannumon123@gmail.com>
2024-03-15 15:37:34 +0800

    Refactor pet page components and add new pet form

--- app/(root)/(routes)/pets/[petId]/page.tsx
+++ app/(root)/(routes)/pets/[petId]/page.tsx
@@ -1,3 +1,13 @@
-export default function NewPet() {
-  return <>dasd</>;
+import PetForm from "./components/pet-form";
+
+export default function PetPage() {
+  return (
+    <>
+      <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <PetForm />
+        </div>
+      </div>
+    </>
+  );
 }
--- app/(root)/(routes)/pets/page.tsx
+++ app/(root)/(routes)/pets/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PetCarousel } from "./components/pet-carousel";
+import { useRouter } from "next/navigation";
+
 export default function Pets() {
-  return <>sasd</>;
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-center gap-10 py-8">
+      <Button
+        variant={"outline"}
+        onClick={() => router.push(`pets/new`)}
+      >
+        Add Pet
+      </Button>
+      <div>Pet Dashboard</div>
+      <div>
+        <PetCarousel />
+      </div>
+    </div>
+  );
 }
